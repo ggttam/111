392f4ff41996ada2a1b00d255cbfc97035dd9ba6
ggttam <62035605+ggttam@users.noreply.github.com>
2025-01-25 20:40:24 +0800

    更新 location.js

--- location.js
+++ location.js
@@ -7,8 +7,6 @@ var obj = JSON.parse(body);
 var title = obj['regionName'] + obj['city']; 
 var subtitle = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' 
-+ "城市" + ":" + obj['city'] + '\n' 
-+ "ip" + ":" + obj['query'] + '\n' 
 + "数据中心" + ":" + obj['org'];
 
-$done({title, subtitle, ip, description});
+$done({title, subtitle, description});
